1a9db9a385512526d66486ddefa6213903c32b44
amber <amber.houle3@gmail.com>
2014-03-24 22:17:28 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flightfinder::Application.config.secret_key_base = 'c2e332dad47c222ef77c7c4d3e449cf38d2d44ad4f52e2991eb51ec1794f254ff353473b25e4c8c675d5e106760eddda09cb9b2243d4bff4e9f37dc1f4d52611'
